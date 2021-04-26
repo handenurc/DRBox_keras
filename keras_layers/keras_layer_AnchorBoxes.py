ee31ed4977381fa0d126b59c3e772a65c757e3fd
Handenur Caliskan <hac129@pitt.edu>
2021-04-26 10:31:29 +0300

    Second Commit

--- keras_layers/keras_layer_AnchorBoxes.py
+++ keras_layers/keras_layer_AnchorBoxes.py
@@ -135,7 +135,7 @@ class AnchorBoxes(Layer):
         wha_list = np.array(wha_list)
 
         # We need the shape of the input tensor
-        batch_size, feature_map_height, feature_map_width, feature_map_channels = x._keras_shape
+        batch_size, feature_map_height, feature_map_width, feature_map_channels = x.shape
 
         # Compute the grid of box center points. They are identical for all aspect ratios.
 
